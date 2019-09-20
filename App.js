59cda393b6d597ec61c586b926f3036a9851c0f0
SYED ALEEM HUSSAIN <49521238+sahzaleem123@users.noreply.github.com>
2019-09-20 23:36:36 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import User from './components/User';
+
+class App extends Component {
+  
+
+  render() {
+    return (
+      <div>
+        <User></User>
+      </div>
+    );
+  }
+}
+
+export default App;
